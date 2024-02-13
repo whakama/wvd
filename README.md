145201173fb4432b48cbca9cc00a98970e3eaea3
shy <70299052+whakama@users.noreply.github.com>
2024-02-13 03:22:17 -0600

    cleaned up and open sourced

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # wine version downloader (wvd)
 
-just a simple script that allows you to download various wine versions from playonlinux's downloader's api without having to use playonlinux itself
+just a simple script that allows you to download various wine versions from playonlinux's downloader's api without having to use playonlinux itself  
+this is old-ish code from early 2023, but i figured id open source it incase someone can make use of it (and its just kinda cool)
+
+assumes you're on a unix like operating system with tar installed, only downloads linux versions of wine
--- index.js
+++ index.js
@@ -1,39 +1,41 @@
+//this is old-ish code, no judge plz
+
 const fs = require('fs')
 const crypto = require('crypto')
 const cp = require('child_process')
 const os = require('os')
 
-var arch = os.arch().replace('x64', 'amd64')
-if(arch !== 'x86' && arch !== 'amd64') return console.log(`your computer's architecture (${arch}) is not supported by wvd`)
+let arch = os.arch().replace('x64', 'amd64')
+if (arch !== 'x86' && arch !== 'amd64') return console.log(`your computer's architecture (${arch}) is not supported by wvd`);
 
-var commands = {
+let commands = {
     '--fetch': fetchWineVersions,
     '--download': downloadWine,
     '--search': searchWineVersions,
     '--help': helpCommand
 }
 
-var command;
-var commandArg;
+let command;
+let commandArg;
 
 for (let i = 0; i < process.argv.length; i++) {
-    if(process.argv[i].startsWith('--')) {
+    if (process.argv[i].startsWith('--')) {
         command = process.argv[i]
-        commandArg = process.argv.slice(i+1).join(' ')
+        commandArg = process.argv.slice(i + 1).join(' ')
         break;
     }
 }
 
-var wineVersions;
-var tempDirPath;
+let wineVersions;
+let tempDirPath;
 
 async function load() {
-    if(!fs.existsSync('./downloads')) fs.mkdirSync('./downloads')
-    tempDirPath = `/tmp/wvd`
-    if(!fs.existsSync(tempDirPath)) fs.mkdirSync(tempDirPath) //tmp dir for downloaded tars
+    if (!fs.existsSync(`${__dirname}/downloads`)) fs.mkdirSync(`${__dirname}/downloads`)
+    tempDirPath = '/tmp/wvd'
+    if (!fs.existsSync(tempDirPath)) fs.mkdirSync(tempDirPath) //tmp dir for downloaded tars
     wineVersions = fs.existsSync(`${tempDirPath}/versions.json`) ? JSON.parse(fs.readFileSync(`${tempDirPath}/versions.json`).toString()) : await fetchWineVersions()
 
-    if(commands[command]) {
+    if (commands[command]) {
         commands[command](commandArg) //readable code :)
     } else {
         helpCommand()
@@ -42,20 +44,26 @@ async function load() {
 
 
 function helpCommand() {
-    console.log('usage:\n\nwvd --fetch: re-fetches wine version list cache (which is cleared at every startup anyways)\nwvd --search (args): lets you search for wine versions\nwvd --download (args): lets you download any of the versions in the list\nwvd --help: man you know what it does')
+    let helpString = 'usage:\n\n' +
+    'wvd --fetch: re-fetches wine version list cache (which is cleared at every startup anyways)\n' +
+    'wvd --search (args): lets you search for wine versions\n' +
+    'wvd --download (args): lets you download any of the versions in the list\n' +
+    'wvd --help: displays this help message'
+
+    console.log(helpString)
 }
 
 async function fetchWineVersions(force) { //force does the exact opposite of what youd expect
-    if(fs.existsSync(`${tempDirPath}/versions.json`) && force) return wineVersions;
+    if (fs.existsSync(`${tempDirPath}/versions.json`) && force) return wineVersions;
 
     console.log(`fetching wine version list... ${force ? '(this only happens every restart)' : ''}`)
-    var wineVersions = await (await fetch('https://phoenicis.playonlinux.com/index.php/wine?os=linux')).json()
+    let wineVersions = await (await fetch('https://phoenicis.playonlinux.com/index.php/wine?os=linux')).json()
 
-    var wineList = []
+    let wineList = []
     for (let i = 0; i < wineVersions.length; i++) {
         for (let j = 0; j < wineVersions[i].packages.length; j++) {
-            var winePackage = wineVersions[i].packages[j]
-            if(wineVersions[i].name.includes('staging')) winePackage.version += `-staging`
+            let winePackage = wineVersions[i].packages[j]
+            if (wineVersions[i].name.includes('staging')) winePackage.version += `-staging`
             winePackage.version += `-${wineVersions[i].name.split('-').pop()}` //add arch to end of wine version name
             wineList.push(winePackage)
         }
@@ -69,52 +77,53 @@ async function fetchWineVersions(force) { //force does the exact opposite of wha
 }
 
 async function searchWineVersions(query) {
-    var results = []
+    let results = []
     for (let i = 0; i < wineVersions.length; i++) {
-        if(wineVersions[i].version.toLowerCase().includes(query.toLowerCase())) results.push(wineVersions[i].version)
+        if (wineVersions[i].version.toLowerCase().includes(query.toLowerCase())) {
+            results.push(wineVersions[i].version)
+        }
     }
 
     results.sort()
 
-    var resultsString = results.join('\n')
-
-    return console.log(resultsString);
+    let resultsString = results.join('\n')
+    console.log(resultsString)
 }
 
 async function downloadWine(version) {
-    var versionObject;
+    let versionObject;
     for (let i = 0; i < wineVersions.length; i++) {
-        if(wineVersions[i].version === version || wineVersions[i].version.replace(`-${arch}`, '') === version) { //assumes based off your computer's arch if you dont specify
-            if(wineVersions[i].version !== version && wineVersions[i].version.replace(`-${arch}`, '') === version) console.log(`you didnt specify an architecture, so assuming ${arch} based on your computer`)
+        if (wineVersions[i].version == version || wineVersions[i].version.replace(`-${arch}`, '') == version) { //assumes based off your computer's arch if you dont specify
+            if (wineVersions[i].version != version && wineVersions[i].version.replace(`-${arch}`, '') == version) console.log(`you didnt specify an architecture, so assuming ${arch} based on your computer`)
             versionObject = wineVersions[i]
             break;
-        } else if(i == wineVersions.length - 1) {
-            console.log('wine version not found, exiting')
-            return;
+        } else if (i == wineVersions.length - 1) {
+            console.log('wine version not found, exiting with code 1')
+            return process.exit(1);
         }
     }
 
-    var wineInstallDir = `${__dirname}/downloads/wine-${versionObject.version}`
+    let wineInstallDir = `${__dirname}/downloads/wine-${versionObject.version}`
 
-    if(fs.existsSync(wineInstallDir)) return console.log(`this version is already installed at ${wineInstallDir}`)
+    if (fs.existsSync(wineInstallDir)) return console.log(`this version is already installed at ${wineInstallDir}`);
 
     console.log(`downloading wine version ${versionObject.version}`)
 
-    var buffer = Buffer.from(await (await fetch(versionObject.url)).arrayBuffer())
+    let buffer = Buffer.from(await (await fetch(versionObject.url)).arrayBuffer())
 
     console.log('verifying...')
-    var hashSum = crypto.createHash('sha1')
+    let hashSum = crypto.createHash('sha1')
     hashSum.update(buffer)
-    var hash = hashSum.digest('hex')
-    if(hash != versionObject.sha1sum) {
+    let hash = hashSum.digest('hex')
+    if (hash != versionObject.sha1sum) {
         console.log('sha1 sum mismatch (try again maybe?)')
         fs.rmSync(`${tempDirPath}/${fileName}`)
-        return console.log('exiting')
+        console.log('exiting with code 1')
+        return process.exit(1);
     }
 
-    var fileName = versionObject.url.split('/').pop()
+    let fileName = versionObject.url.split('/').pop()
     fs.writeFileSync(`${tempDirPath}/${fileName}`, buffer)
-    
 
     console.log('extracting wine...')
     fs.mkdirSync(wineInstallDir)
